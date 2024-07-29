308a3cf392e2ac179d57b1e481fde03297fbb999
Hyunjoon Kim <rickykhj@korea.ac.kr>
2024-07-30 00:31:15 +0900

    Minor refactoring

--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,14 @@
 import * as net from "net";
 
-type role = "master" | "slave" | "sentinel";
 type ServerConfig = {
   port: number;
-  role: role;
+  role: "master" | "slave" | "sentinel";
   master_replid: string;
   master_repl_offset: number;
 };
 const values = new Map<string, string>();
 
-const randomString = () => {
+const randomString = (): string => {
   const chars =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
@@ -19,7 +18,7 @@ const randomString = () => {
   return result;
 };
 
-const parseArgs = () => {
+const parseArgs = (): Map<string, string> => {
   const args = process.argv.slice(2);
   const argObj = new Map<string, string>();
 
@@ -35,7 +34,7 @@ const parseArgs = () => {
   return argObj;
 };
 
-const args = parseArgs();
+const args: Map<string, string> = parseArgs();
 const cfg: ServerConfig = {
   port: +(args.get("port") ?? 6379),
   role: args.has("replicaof") ? "slave" : "master",
@@ -43,9 +42,9 @@ const cfg: ServerConfig = {
   master_repl_offset: 0,
 };
 
-function parseRESP(input: string): string[] {
+const parseRESP = (input: string): string[] => {
   const lines = input.split("\r\n");
-  let result: string[] = [];
+  let result = [];
   let index = 0;
 
   if (lines[index].startsWith("*")) {
@@ -76,70 +75,88 @@ function parseRESP(input: string): string[] {
   }
 
   return result;
-}
+};
 
+const pingHandler = (commands: string[], connection: net.Socket): void => {
+  if (commands.length === 1) {
+    connection.write("+PONG\r\n");
+  } else {
+    if (commands.length > 2) {
+      connection.write("-ERR wrong number of arguments for command\r\n");
+    }
+    connection.write(`+${commands[1]}\r\n`);
+  }
+};
+
+const echoHandler = (commands: string[], connection: net.Socket): void => {
+  if (commands.length !== 2) {
+    connection.write("-ERR wrong number of arguments for command\r\n");
+  }
+  connection.write(`+${commands[1]}\r\n`);
+};
+
+const setHandler = (commands: string[], connection: net.Socket): void => {
+  if (commands.length < 3) {
+    connection.write("-ERR wrong number of arguments for command\r\n");
+  }
+  values.set(commands[1], commands[2]);
+  if (!!commands[3] && commands[3].toUpperCase() === "PX") {
+    setTimeout(() => {
+      values.delete(commands[1]);
+    }, +commands[4]);
+  }
+  connection.write("+OK\r\n");
+};
+
+const getHandler = (commands: string[], connection: net.Socket): void => {
+  if (commands.length !== 2) {
+    connection.write("-ERR wrong number of arguments for command\r\n");
+  }
+  const value = values.get(commands[1]);
+  connection.write(value ? `\$${value.length}\r\n${value}\r\n` : "$-1\r\n");
+};
+
+const infoHandler = (commands: string[], connection: net.Socket): void => {
+  if (commands.length < 2) {
+    connection.write("-ERR wrong number of arguments for command\r\n");
+  }
+  for (const section of commands.slice(1)) {
+    switch (section) {
+      case "replication":
+        const response = `role:${cfg.role}\r\nmaster_replid:${cfg.master_replid}\r\nmaster_repl_offset:${cfg.master_repl_offset}`;
+        const bulkString = `$${response.length}\r\n${response}\r\n`;
+        connection.write(bulkString);
+        break;
+      default:
+        connection.write("-ERR unknown section\r\n");
+    }
+  }
+};
+
+const commandHandlers: Map<
+  string,
+  (commands: string[], connection: net.Socket) => void
+> = new Map([
+  ["PING", pingHandler],
+  ["ECHO", echoHandler],
+  ["SET", setHandler],
+  ["GET", getHandler],
+  ["INFO", infoHandler],
+]);
+
+// TODO: Folder Structuring
 const server: net.Server = net.createServer((connection: net.Socket) => {
   console.log("Client Connected");
 
   connection.on("data", (buffer) => {
     const commands = parseRESP(buffer.toString());
+    const command = commands[0].toUpperCase();
 
-    switch (commands[0].toUpperCase()) {
-      case "PING":
-        if (commands.length === 1) {
-          connection.write("+PONG\r\n");
-        } else {
-          if (commands.length > 2) {
-            connection.write("-ERR wrong number of arguments for command\r\n");
-          }
-          connection.write(`+${commands[1]}\r\n`);
-        }
-        break;
-      case "ECHO":
-        if (commands.length !== 2) {
-          connection.write("-ERR wrong number of arguments for command\r\n");
-        }
-        connection.write(`+${commands[1]}\r\n`);
-        break;
-      case "SET":
-        if (commands.length < 3) {
-          connection.write("-ERR wrong number of arguments for command\r\n");
-        }
-        values.set(commands[1], commands[2]);
-        if (!!commands[3] && commands[3].toUpperCase() === "PX") {
-          setTimeout(() => {
-            values.delete(commands[1]);
-          }, +commands[4]);
-        }
-        connection.write("+OK\r\n");
-        break;
-      case "GET":
-        if (commands.length !== 2) {
-          connection.write("-ERR wrong number of arguments for command\r\n");
-        }
-        const value = values.get(commands[1]);
-        connection.write(
-          value ? `\$${value.length}\r\n${value}\r\n` : "$-1\r\n"
-        );
-        break;
-      case "INFO":
-        if (commands.length < 2) {
-          connection.write("-ERR wrong number of arguments for command\r\n");
-        }
-        for (const section of commands.slice(1)) {
-          switch (section) {
-            case "replication":
-              const response = `role:${cfg.role}\r\nmaster_replid:${cfg.master_replid}\r\nmaster_repl_offset:${cfg.master_repl_offset}`;
-              const bulkString = `$${response.length}\r\n${response}\r\n`;
-              connection.write(bulkString);
-              break;
-            default:
-              connection.write("-ERR unknown section\r\n");
-          }
-        }
-        break;
-      default:
-        connection.write("-ERR unknown command\r\n");
+    if (commandHandlers.has(command)) {
+      const handler = commandHandlers.get(command)!;
+      handler(commands, connection);
+    } else {
+      connection.write("-ERR unknown command\r\n");
     }
   });
 
