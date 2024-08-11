e4633e049d03c8c2bd1d41929052d7b00784cdab
Hyunjoon Kim <rickykhj@korea.ac.kr>
2024-08-11 18:06:33 +0900

    Added empty RDB transferring logic (squashed)

--- app/main.ts
+++ app/main.ts
@@ -165,6 +165,13 @@ const replconfHandler = (commands: string[], connection: net.Socket): void => {
 
 const psyncHandler = (commands: string[], connection: net.Socket): void => {
   connection.write(`+FULLRESYNC ${cfg.master_replid} 0\r\n`);
+
+  const emptyRdbHex =
+    "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
+  const emptyRdb = Buffer.from(emptyRdbHex, "hex");
+  connection.write(
+    Buffer.concat([Buffer.from(`$${emptyRdb.length}\r\n`, "utf-8"), emptyRdb])
+  );
 };
 
 const commandHandlers: Map<
